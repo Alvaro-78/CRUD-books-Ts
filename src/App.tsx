ae2e8e13ab2aeafa7f955d196e96eaca290e75e5
alvaro-78 <alvaro78an@gmail.com>
2021-12-20 20:40:55 +0100

    Added project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import AddBook from './pages/AddBook';
+import BookList from './pages/BookList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<BookList />} />
+				<Route path="/add-new-book" element={<AddBook />} />
+				<Route path="/update-book/:id" element={<AddBook />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
